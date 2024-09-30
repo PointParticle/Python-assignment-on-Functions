6435151e8c8b2fb7f689ec73293ac129eee6525d
PointParticle <168024488+PointParticle@users.noreply.github.com>
2024-09-30 09:53:28 +0200

    Update Calculations.py

--- Calculations.py
+++ Calculations.py
@@ -1,6 +1,5 @@
 #Question 1
 def calculate_discount(price, discount_percent):
-    # Check if the discount is 20% or higher
     if discount_percent >= 20:
     discount_amount = price * (discount_percent / 100)
         final_price = price - discount_amount
